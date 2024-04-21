2dd8cf0fbf1ac32b40cd154ec6f5111a87338d9a
awais-ur-rehman <awaisjarral37@gmail.com>
2024-04-22 00:22:21 +0500

    added login view

---
+++ lib/presentation/login/login_view.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ocula_care/presentation/login/widgets/login_form.dart';
+
+import '../../configs/presentation/constants/colors.dart';
+import '../../logic/login_cubit/login_cubit.dart';
+import '../../logic/login_cubit/login_cubit_state.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.sizeOf(context).height;
+    double screenWidth = MediaQuery.sizeOf(context).width;
+    return Scaffold(
+      backgroundColor: AppColors.screenBackground,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            context.read<LoginCubit>().dispose();
+            context.pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: AppColors.appColor,
+          ),
+        ),
+      ),
+      body: SizedBox(
+        height: screenHeight,
+        width: screenWidth,
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: BlocBuilder<LoginCubit, LoginState>(
+                builder: (context, state) {
+              if (state is LoginStateLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is LoginStateFailure) {
+                return Center(
+                  child: Text(state.errorMsg),
+                );
+              } else if (state is LoginStateLoaded) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0, vertical: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 32.sp,
+                          color: AppColors.appColor,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                        ),
+                      ),
+                      Text(
+                        'Welcome Back!',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 16.sp,
+                          color: AppColors.textGrey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.03,
+                      ),
+                      LoginForm(
+                        passVisible: state.passVisible,
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.01,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Didn\'t have an account.',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w100,
+                              color: AppColors.textGrey,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 5.0,
+                          ),
+                          Text(
+                            'Signup',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.appColor,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
