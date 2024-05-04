f9feea0f902fd3e262ec8116adb0a58bf4632850
BilalMartin <m.bilalkhant@gmail.com>
2024-05-04 22:45:44 +0500

    removed: unused imports

--- lib/presentation/img_capture/img_capture_view.dart
+++ lib/presentation/img_capture/img_capture_view.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
-
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
@@ -14,8 +11,7 @@ import 'package:OculaCare/logic/image_capture/img_capture_state.dart';
 import 'package:OculaCare/presentation/sign_up/widgets/cstm_flat_btn.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
-
-import '../../configs/utils/utils,dart.dart';
+import '../../configs/utils/utils.dart';
 
 class ImageCaptureScreen extends StatelessWidget {
   const ImageCaptureScreen({Key? key}) : super(key: key);
