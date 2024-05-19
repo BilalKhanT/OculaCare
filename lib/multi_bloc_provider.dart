1fb1709999ac557bf2c34366bf8fed4853e3e930
BilalMartin <m.bilalkhant@gmail.com>
2024-05-19 09:46:42 +0500

    added: pdf-cubit-states

--- lib/multi_bloc_provider.dart
+++ lib/multi_bloc_provider.dart
@@ -1,3 +1,5 @@
+import 'package:OculaCare/logic/patient_profile/patient_profile_cubit.dart';
+import 'package:OculaCare/logic/pdf_cubit/pdf_cubit_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:OculaCare/logic/image_capture/img_capture_cubit.dart';
@@ -25,6 +27,12 @@ class ProvideMultiBloc extends StatelessWidget {
       ),
       BlocProvider(
         create: (context) => OtpCubit(),
+      ),
+      BlocProvider(
+        create: (context) => PDFCubit(),
+      ),
+      BlocProvider(
+        create: (context) => PatientProfileCubit(),
       ),
     ], child: child);
   }
