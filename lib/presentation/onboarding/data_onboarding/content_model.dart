0a1862b004ba106eee854a61b06094765f464fb3
BilalMartin <m.bilalkhant@gmail.com>
2024-05-20 02:30:54 +0500

    added: education-model

--- lib/presentation/onboarding/data_onboarding/content_model.dart
+++ lib/presentation/onboarding/data_onboarding/content_model.dart
@@ -6,6 +6,16 @@ class OnBoardingContent {
   OnBoardingContent({this.image, this.title, this.discription});
 }
 
+class EducationModel {
+  final String title, description, image;
+
+  EducationModel({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
+
 List<OnBoardingContent> contents = [
   OnBoardingContent(
       title: 'Timely Detection',
@@ -22,4 +32,27 @@ List<OnBoardingContent> contents = [
       image: 'assets/images/onboarding_vector.png',
       discription:
           "Industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it "),
+];
+
+List<EducationModel> education = [
+  EducationModel(
+    title: "Eye Health Tips",
+    description: "",
+    image: 'assets/images/edu_1.jpeg',
+  ),
+  EducationModel(
+    title: "Eye Health Tips",
+    description: "",
+    image: 'assets/images/edu_3.jpeg',
+  ),
+  EducationModel(
+    title: "Eye Health Tips",
+    description: "",
+    image: 'assets/images/edu_2.jpeg',
+  ),
+  EducationModel(
+    title: "Eye Health Tips",
+    description: "",
+    image: 'assets/images/edu_1.jpeg',
+  ),
 ];
