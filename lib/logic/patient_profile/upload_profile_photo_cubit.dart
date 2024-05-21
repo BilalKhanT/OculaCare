6fa9fff4277ca137d9109600ec1b53c673f6e344
BilalMartin <m.bilalkhant@gmail.com>
2024-05-21 23:34:19 +0500

    added: image conversion to base64

--- lib/logic/patient_profile/upload_profile_photo_cubit.dart
+++ lib/logic/patient_profile/upload_profile_photo_cubit.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:OculaCare/logic/patient_profile/patient_profile_cubit.dart';
 import 'package:OculaCare/logic/patient_profile/upload_profile_photo_state.dart';
-import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UploadProfilePhotoCubit extends Cubit<UploadProfilePhotoState> {
@@ -7,12 +11,14 @@ class UploadProfilePhotoCubit extends Cubit<UploadProfilePhotoState> {
 
   XFile? image;
 
-  uploadPhoto() async {
+  uploadPhoto(BuildContext context) async {
     emit(UploadProfilePhotoStateLoading());
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       image = pickedFile;
+      final base64Image = base64Encode(await File(pickedFile.path).readAsBytes());
+      context.read<PatientProfileCubit>().setImage(base64Image);
       emit(UploadProfilePhotoStateLoaded(pickedFile));
     }
     else {
