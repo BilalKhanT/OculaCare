d0e4fbef8a8795a7b540fa02c81e2aa2cc905065
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-08 03:46:30 +0500

    added: test schedule tab state

---
+++ lib/logic/tests/test_schedule_tab_state.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+
+sealed class ScheduleTabState extends Equatable {
+  const ScheduleTabState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ScheduleTabInitial extends ScheduleTabState {}
+
+final class ScheduleTabToggled extends ScheduleTabState {
+  final bool isVision;
+  final bool isColor;
+
+  const ScheduleTabToggled(
+      this.isVision,
+      this.isColor,
+      );
+
+  @override
+  List<Object> get props => [isVision, isColor];
+}
