a9bf10ca803936c5b01a9eb4764a0ec80f26afa3
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-10 02:08:12 +0500

    removed comments

--- lib/data/therapies_data/cataract_therapies.dart
+++ lib/data/therapies_data/cataract_therapies.dart
@@ -2,11 +2,11 @@ final List<Map<String, dynamic>> therapiesCataract = [
   // Acupressure Therapy
   {
     "title": "Acupressure",
-    "timeLimit": 2, // Total duration in minutes (can be modified)
-    "type": "instruction", // Specifies that this therapy is instructional
+    "timeLimit": 2,
+    "type": "instruction",
     "svgPath": "assets/images/cataracts/acupressure.png",
     "sound":
-        "assets/audio/relaxing_breathing.mp3", // Optional, remove if no sound is needed
+        "assets/audio/relaxing_breathing.mp3",
     "benefits": [
       "Promotes circulation around the eyes",
       "Reduces eye tension",
@@ -17,27 +17,26 @@ final List<Map<String, dynamic>> therapiesCataract = [
         "step": 1,
         "instruction": "Find the spot just behind your earlobe.",
         "svgPath": "assets/images/cataracts/earlobe.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10
       },
       {
         "step": 2,
         "instruction":
             "Find the point that is the width of 1 thumb behind that.",
         "svgPath": "assets/images/cataracts/step2.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10
       },
       {
         "step": 3,
         "instruction": "Rub it gently 5 times.",
         "svgPath": "assets/images/cataracts/rub.png",
-        "duration": 30 // Duration in seconds
+        "duration": 30
       },
       {
         "step": 4,
         "instruction": "Repeat 3 times a day for 30 days.",
         "svgPath": "assets/images/cataracts/acupressure.png",
-        "duration":
-            60 // Duration in seconds (can be adjusted based on therapy flow)
+        "duration": 60
       }
     ]
   },
@@ -45,8 +44,8 @@ final List<Map<String, dynamic>> therapiesCataract = [
   // Palming
   {
     "title": "Palming",
-    "timeLimit": 5, // Total duration in minutes
-    "type": "image", // Specifies that this therapy only requires images
+    "timeLimit": 5,
+    "type": "image",
     "svgPath": "assets/images/palming/palming.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": [
@@ -59,13 +58,13 @@ final List<Map<String, dynamic>> therapiesCataract = [
         "step": 1,
         "instruction": "Rub your palms together until warm.",
         "svgPath": "assets/images/palming/rub_palms.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10
       },
       {
         "step": 2,
         "instruction": "Place your warm palms over your eyes.",
         "svgPath": "assets/images/palming/coverEyes.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10
       },
       {
         "step": 3,
@@ -85,8 +84,8 @@ final List<Map<String, dynamic>> therapiesCataract = [
   // Focus Shifting
   {
     "title": "Focus Shifting",
-    "timeLimit": 3, // Total duration in minutes
-    "type": "image", // Specifies that this therapy only requires images
+    "timeLimit": 3,
+    "type": "image",
     "svgPath": "assets/images/focus_shifting/focus_shifting.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": [
@@ -98,31 +97,31 @@ final List<Map<String, dynamic>> therapiesCataract = [
         "step": 1,
         "instruction": "Hold your thumb about 10 inches away from your face.",
         "svgPath": "assets/images/focus_shifting/thumb.png",
-        "duration": 20 // Duration in seconds
+        "duration": 20
       },
       {
         "step": 2,
         "instruction": "Focus on your thumb for a few seconds.",
         "svgPath": "assets/images/focus_shifting/focus_thumb.png",
-        "duration": 30 // Duration in seconds
+        "duration": 30
       },
       {
         "step": 3,
         "instruction": "Shift your focus to something farther away.",
         "svgPath": "assets/images/distance_gazing/far.png",
-        "duration": 30 // Duration in seconds
+        "duration": 30
       },
       {
         "step": 4,
         "instruction": "Shift your focus back to your thumb.",
         "svgPath": "assets/images/focus_shifting/focus_thumb.png",
-        "duration": 30 // Duration in seconds
+        "duration": 30
       },
       {
         "step": 5,
         "instruction": "Repeat the focus shifting.",
         "svgPath": "assets/images/focus_shifting/thumb.png",
-        "duration": 30 // Duration in seconds
+        "duration": 30
       },
     ]
   },
@@ -130,11 +129,11 @@ final List<Map<String, dynamic>> therapiesCataract = [
   //eye rolling
   {
     "title": "Eye Rolling",
-    "timeLimit": 2, // Total duration in minutes
+    "timeLimit": 2,
     "type":
-        "animation_eye_rolling", // Specifies that this therapy requires Rive animation
+        "animation_eye_rolling",
     "svgPath":
-        "assets/images/eye_rolling/eye_rolling.png", // Used for the first step image
+        "assets/images/eye_rolling/eye_rolling.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": [
       "Strengthens eye muscles",
@@ -146,9 +145,8 @@ final List<Map<String, dynamic>> therapiesCataract = [
         "step": 1,
         "instruction": "Roll your eyes in a circular motion.",
         "svgPath":
-            "assets/images/eye_rolling/roll_eyes.png", // Image for first step
-        "duration":
-            10 // Duration in seconds for step 1 before transitioning to the animation
+            "assets/images/eye_rolling/roll_eyes.png",
+        "duration": 10
       }
     ]
   }
