5f09bf20d1f450ba0cd0f272361f72aa765f5387
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-10 02:08:19 +0500

    removed comments

--- lib/data/therapies_data/crossed_eye_therapies.dart
+++ lib/data/therapies_data/crossed_eye_therapies.dart
@@ -2,8 +2,8 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Brock String Exercise
   {
     "title": "Brock String Exercise",
-    "timeLimit": 10, // Total duration in minutes
-    "type": "animation_string", // Specifies that this therapy is instructional
+    "timeLimit": 10, 
+    "type": "animation_string",
     "svgPath": "assets/images/crossed_eyes/brock_string/brock_string.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Improves eye alignment", "Strengthens eye coordination"],
@@ -13,20 +13,20 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "instruction":
             "Look at the string with three beads by placing the phone in front of your nose.",
         "svgPath": "assets/images/crossed_eyes/brock_string/brock_string.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 2,
         "instruction":
             "Focus on the nearest bead, then shift to the middle, and then the farthest bead.",
         "svgPath": "",
-        "duration": 30 // Duration in seconds
+        "duration": 30 
       },
       {
         "step": 3,
         "instruction": "Ensure both eyes are focusing on the same bead.",
         "svgPath": "",
-        "duration": 30 // Duration in seconds
+        "duration": 30 
       }
     ]
   },
@@ -34,8 +34,8 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Mirror Eye Stretch
   {
     "title": "Mirror Eye Stretch",
-    "timeLimit": 5, // Total duration in minutes
-    "type": "instruction", // Specifies that this therapy is instructional
+    "timeLimit": 5, 
+    "type": "instruction",
     "svgPath": "assets/images/crossed_eyes/mirror_eye/mirror_eye.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Strengthens eye muscles", "Improves coordination"],
@@ -45,20 +45,20 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "instruction":
             "Look at the screen and cover your dominant eye and follow the object.",
         "svgPath": "assets/images/crossed_eyes/mirror_eye/coverEyes.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 2,
         "instruction":
             "Move your gaze left, right, up, and down while focusing on your reflection.",
         "svgPath": "assets/images/crossed_eyes/mirror_eye/bg.png",
-        "duration": 20 // Duration in seconds
+        "duration": 20 
       },
       {
         "step": 3,
         "instruction": "Alternate between covering each eye and repeat.",
         "svgPath": "assets/images/crossed_eyes/mirror_eye/bg.png",
-        "duration": 20 // Duration in seconds
+        "duration": 20 
       }
     ]
   },
@@ -66,8 +66,8 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Pencil Push-Up Therapy
   {
     "title": "Pencil Push-Up",
-    "timeLimit": 10, // Total duration in minutes
-    "type": "instruction", // Specifies that this therapy is instructional
+    "timeLimit": 10, 
+    "type": "instruction",
     "svgPath": "assets/images/crossed_eyes/pencil_pushups/pencil_pushups.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Improves focus", "Strengthens eye coordination"],
@@ -77,7 +77,7 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "instruction": "Hold a pencil at arm's length in front of your nose.",
         "svgPath":
             "assets/images/crossed_eyes/pencil_pushups/pencil_pushups.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 2,
@@ -85,28 +85,28 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
             "Slowly move the pencil closer, keeping your focus on it.",
         "svgPath":
             "assets/images/crossed_eyes/pencil_pushups/bring_pencil_closer.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 3,
         "instruction": "Stop when the pencil becomes blurry or you see double.",
         "svgPath":
             "assets/images/crossed_eyes/pencil_pushups/stop_when_blurry.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 4,
         "instruction": "Move the pencil back to arm's length and repeat.",
         "svgPath":
             "assets/images/crossed_eyes/pencil_pushups/pencil_pushups.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 4,
         "instruction": "continue this exercise for a minute.",
         "svgPath":
             "assets/images/crossed_eyes/pencil_pushups/pencil_pushups.png",
-        "duration": 60 // Duration in seconds
+        "duration": 60 
       }
     ]
   },
@@ -114,8 +114,8 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Barrel Card Exercise
   {
     "title": "Barrel Card Exercise",
-    "timeLimit": 5, // Total duration in minutes
-    "type": "animation_barrel", // Specifies that this therapy is instructional
+    "timeLimit": 5, 
+    "type": "animation_barrel",
     "svgPath": "assets/images/crossed_eyes/barrel_card/barrel_card.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Improves eye convergence", "Strengthens eye coordination"],
@@ -125,32 +125,32 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "instruction":
             "Look at the card with three barrels in front of your nose.",
         "svgPath": "assets/images/crossed_eyes/barrel_card/barrel_bg.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 2,
         "instruction": "Focus on the largest barrel.",
         "svgPath": "assets/images/crossed_eyes/barrel_card/largest.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 3,
         "instruction": "Focus on the middle.",
         "svgPath": "assets/images/crossed_eyes/barrel_card/middle.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 4,
         "instruction": "Focus on the smallest.",
         "svgPath": "assets/images/crossed_eyes/barrel_card/smallest.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 5,
         "instruction":
             "Move the card away from your nose while focusing on the barrels.",
         "svgPath": "assets/images/crossed_eyes/barrel_card/barrel_bg.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       }
     ]
   },
@@ -158,8 +158,8 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Eye Patch Therapy
   {
     "title": "Eye Patch Therapy",
-    "timeLimit": 1, // Total duration in minutes
-    "type": "instruction", // Specifies that this therapy is instructional
+    "timeLimit": 1, 
+    "type": "instruction",
     "svgPath": "assets/images/crossed_eyes/eye_patch/eye_patch.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Strengthens the weaker eye", "Improves coordination"],
@@ -168,14 +168,13 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "step": 1,
         "instruction": "Cover your stronger eye with an eye patch.",
         "svgPath": "assets/images/crossed_eyes/eye_patch/eye_patch.png",
-        "duration":
-            5 // Duration in seconds (this could vary based on recommendations)
+        "duration": 5
       },
       {
         "step": 2,
         "instruction": "Use your weaker eye to read the text.",
         "svgPath": "assets/images/crossed_eyes/eye_patch/read.png",
-        "duration": 45 // Duration in seconds
+        "duration": 45 
       }
     ]
   },
@@ -183,9 +182,9 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
   // Peripheral Awareness Exercise
   {
     "title": "Peripheral Awareness Exercise",
-    "timeLimit": 5, // Total duration in minutes
+    "timeLimit": 5,
     "type":
-        "animation_peripheral", // Specifies that this therapy is instructional
+        "animation_peripheral",
     "svgPath": "assets/images/crossed_eyes/peripheral_awareness/peripheral.png",
     "sound": "assets/audio/relaxing_breathing.mp3",
     "benefits": ["Strengthens peripheral vision", "Improves coordination"],
@@ -195,14 +194,14 @@ final List<Map<String, dynamic>> crossedEyeTherapies = [
         "instruction":
             "Hold your phone in landscape and look at the person in the center.",
         "svgPath": "assets/images/crossed_eyes/peripheral_awareness/step01.png",
-        "duration": 10 // Duration in seconds
+        "duration": 10 
       },
       {
         "step": 2,
         "instruction":
             "Without moving your head, shift your gaze left, right, up, and down.",
         "svgPath": "assets/images/crossed_eyes/peripheral_awareness/step02.png",
-        "duration": 20 // Duration in seconds
+        "duration": 20 
       }
     ]
   }
