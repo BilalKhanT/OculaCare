6d94be74447e9721dcfbb395bd57073dc20b2d3f
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-10 00:30:43 +0500

    added custom tile

---
+++ lib/presentation/therapy/widgets_therapy/custom_therapy_tile.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import '../../../configs/presentation/constants/colors.dart';
+
+class TherapyTile extends StatelessWidget {
+  final Map<String, dynamic> therapy;
+  final double screenHeight;
+  final VoidCallback onTap;
+
+  const TherapyTile({
+    required this.therapy,
+    required this.screenHeight,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: screenHeight * 0.12,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 5,
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(
+                  therapy['svgPath'],
+                  width: screenHeight * 0.1,
+                  height: screenHeight * 0.1,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      therapy['title'],
+                      style: TextStyle(
+                        color: AppColors.textPrimary,
+                        fontFamily: 'MontserratMedium',
+                        fontWeight: FontWeight.w700,
+                        fontSize: MediaQuery.of(context).size.width * 0.035,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: AppColors.appColor,
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Text(
+                        "${therapy['timeLimit']} min",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
