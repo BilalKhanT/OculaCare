3484b5db227784593c68f6dca900fdd177453d19
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-09 12:55:35 +0500

    updated: test model

--- lib/data/models/tests/test_result_model.dart
+++ lib/data/models/tests/test_result_model.dart
@@ -3,7 +3,7 @@ class TestResultModel {
   final String date;
   final String testType;
   final String testName;
-  final double testScore;
+  final int testScore;
   final String resultDescription;
   final String recommendation;
   final String precautions;
@@ -18,4 +18,30 @@ class TestResultModel {
     required this.recommendation,
     required this.precautions,
   });
+
+  factory TestResultModel.fromJson(Map<String, dynamic> json) {
+    return TestResultModel(
+      patientName: json['patient_name'] ?? '',
+      date: json['date'] ?? '',
+      testType: json['test_type'] ?? '',
+      testName: json['test_name'] ?? '',
+      testScore: json['test_score'] ?? 0,
+      resultDescription: json['result_description'] ?? '',
+      recommendation: json['test_recommendations'] ?? '',
+      precautions: json['test_impact'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'patient_name': patientName,
+      'date': date,
+      'test_type': testType,
+      'test_name': testName,
+      'test_score': testScore,
+      'result_description': resultDescription,
+      'test_recommendations': recommendation,
+      'test_impact': precautions,
+    };
+  }
 }
