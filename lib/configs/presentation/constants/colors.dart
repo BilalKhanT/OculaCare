18874b6391e95eed33be7162baaab1b3ebe22a33
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-10 01:56:06 +0500

    updated: colors file

--- lib/configs/presentation/constants/colors.dart
+++ lib/configs/presentation/constants/colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const appColor = Color(0xFF034694);
   static const whiteColor = Color(0xFFFFFFFF);
   static final textGrey = Colors.grey.shade700;
+  static const secondaryText = Color(0xFF636363);
 }
