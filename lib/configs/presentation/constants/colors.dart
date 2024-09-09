531111314bfa399c7a70dd61bbae756040f0072c
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-10 02:05:56 +0500

    updated: colors

--- lib/configs/presentation/constants/colors.dart
+++ lib/configs/presentation/constants/colors.dart
@@ -6,5 +6,9 @@ class AppColors {
   static const appColor = Color(0xFF034694);
   static const whiteColor = Color(0xFFFFFFFF);
   static final textGrey = Colors.grey.shade700;
+  static const Color textPrimary = Colors.black; // Black
+  static const Color textSecondary = Colors.black54; // Light Black/Gray
+  static const Color backgroundTherapy = Color(0xFF1E1B24); // therapy color
+  static const Color textTherapy = Color(0xFFfef3d6);
   static const secondaryText = Color(0xFF636363);
 }
