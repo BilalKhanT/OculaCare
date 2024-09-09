f29d770232dd56fb94f4652b3d782fc59c4fb725
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-09 12:54:55 +0500

    updated: isihara cubit

--- lib/logic/tests/color_tests/isihara_cubit.dart
+++ lib/logic/tests/color_tests/isihara_cubit.dart
@@ -1,13 +1,23 @@
 import 'dart:math';
+import 'package:OculaCare/configs/app/remote/ml_model.dart';
+import 'package:OculaCare/data/models/tests/test_result_model.dart';
+import 'package:OculaCare/data/repositories/local/preferences/shared_prefs.dart';
+import 'package:OculaCare/data/repositories/tests/test_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc/bloc.dart';
+import 'package:intl/intl.dart';
+
+import '../../../data/models/api_response/response_model.dart';
 
 class IshiharaCubit extends Cubit<IshiharaState> {
+  final TestRepository testRepo = TestRepository();
   final Random _random = Random();
   final AudioPlayer _audioPlayer = AudioPlayer();
   final AudioPlayer _successPlayer = AudioPlayer();
   final AudioPlayer _errorPlayer = AudioPlayer();
+  String analysis = '';
+  MlModel ml = MlModel();
   int? answerSelected;
 
   IshiharaCubit()
@@ -20,12 +30,18 @@ class IshiharaCubit extends Cubit<IshiharaState> {
           plates: [],
         ));
 
+  String getCurrentDateString() {
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('dd-MM-yyyy');
+    String formattedDate = formatter.format(now);
+    return formattedDate;
+  }
+
   void startGame() {
     _initializePlates();
     _startBackgroundAudio();
   }
 
-  // Start playing the background audio on loop.
   void _startBackgroundAudio() async {
     await _audioPlayer.setSource(AssetSource('audio/good-night-160166.mp3'));
     await _audioPlayer.setVolume(0.1);
@@ -33,7 +49,6 @@ class IshiharaCubit extends Cubit<IshiharaState> {
     await _audioPlayer.resume();
   }
 
-  // Initialize the plates with random numbers in the constructor body
   void _initializePlates() {
     final List<int> plates =
         List.generate(10, (index) => _random.nextInt(90) + 10);
@@ -62,7 +77,14 @@ class IshiharaCubit extends Cubit<IshiharaState> {
         answerSelected = null;
         closeGame();
         emit(state.copyWith(loading: true));
-        await Future.delayed(const Duration(seconds: 4));
+        final String date = getCurrentDateString();
+        ResponseModel response = await ml.getData(
+            'The Ishihara test is a color vision test that detects color blindness by having individuals identify numbers or patterns within a series of colored plates. The test consists of 10 questions, where each correct answer indicates the ability to distinguish specific colors.The patient recently took the Ishihara test and scored ${state.correctAnswers} out of 10.Based on this score, please provide a small analysis in 2 lines of the patient color vision without heading also generate text in such a way that youre talking to the patient directly. Consider if the score indicates normal color vision(score 9-10), mild(score 7-8), moderate(score 4-6).');
+        ResponseModel resp = await ml.getData('Also, provide recommendations in form of points without any heading only 3 points, also generate text in such a way that youre talking to the patient directly.');
+        ResponseModel resp_ = await ml.getData('Additionally, mention any potential impacts of color blindness in daily activities without heading and only 3 points, also generate text in such a way that youre talking to the patient directly');
+        TestResultModel data = TestResultModel(patientName: sharedPrefs.userName, date: date, testType: 'Color Perception Test', testName: 'Isihara Plates', testScore: state.correctAnswers, resultDescription: response.text, recommendation: resp.text, precautions: resp_.text);
+        bool flag = await testRepo.addTestRecord(data);
+        print(flag);
         emit(state.copyWith(testCompleted: true, loading: false));
       } else {
         answerSelected = null;
@@ -152,4 +174,5 @@ class IshiharaState {
       loading: loading ?? this.loading,
     );
   }
+
 }
