da6f33f1dbceb3e9d5ba25ea4fc9616a23fa193d
BilalMartin <m.bilalkhant@gmail.com>
2024-09-10 23:40:55 +0500

    chore: replaced back icon

--- lib/presentation/test_dashboard/color_perception_tests/ishihara_test_view.dart
+++ lib/presentation/test_dashboard/color_perception_tests/ishihara_test_view.dart
@@ -44,7 +44,7 @@ class IshiharaScreen extends StatelessWidget {
               context.pop();
             },
             icon: const Icon(
-              Icons.arrow_back,
+              Icons.arrow_back_ios_new,
               color: AppColors.appColor,
               size: 30.0,
             ),
