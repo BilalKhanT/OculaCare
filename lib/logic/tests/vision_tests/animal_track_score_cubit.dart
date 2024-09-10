85777ddd368958bbce0ce5c69f94c86fba3466aa
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-11 04:05:51 +0500

    updated: track score cubit

--- lib/logic/tests/vision_tests/animal_track_score_cubit.dart
+++ lib/logic/tests/vision_tests/animal_track_score_cubit.dart
@@ -2,6 +2,11 @@ import 'package:OculaCare/configs/app/remote/ml_model.dart';
 import 'package:OculaCare/data/repositories/tests/test_repo.dart';
 import 'package:OculaCare/logic/tests/vision_tests/animal_track_score_state.dart';
 import 'package:bloc/bloc.dart';
+import 'package:intl/intl.dart';
+
+import '../../../data/models/api_response/response_model.dart';
+import '../../../data/models/tests/test_result_model.dart';
+import '../../../data/repositories/local/preferences/shared_prefs.dart';
 
 class AnimalTrackScoreCubit extends Cubit<AnimalTrackScoreState> {
   AnimalTrackScoreCubit() : super(AnimalTrackScoreInitial());
@@ -10,10 +15,54 @@ class AnimalTrackScoreCubit extends Cubit<AnimalTrackScoreState> {
   MlModel ml = MlModel();
 
   void emitInitial() {
+    emit(AnimalTrackScoreLoading());
     emit(AnimalTrackScoreInitial());
   }
 
-  Future<void> analyseTrackScore() async {
+  String getCurrentDateString() {
+    DateTime now = DateTime.now();
+    DateFormat formatter = DateFormat('dd-MM-yyyy');
+    String formattedDate = formatter.format(now);
+    return formattedDate;
+  }
 
+  Future<void> analyseTrackScore(int score1, int score2, int score3) async {
+    int totalScore = score1 + score2 + score3;
+    print(totalScore);
+    String analysis = determineVisionAcuity(totalScore);
+    ResponseModel recommendation = await ml.getData(
+        'The vision tracking test measures the ability to follow a moving object at different speeds and sizes. The test has three levels of difficulty, and each level gets progressively harder. The patient completed this test and result was $analysis, provide three recommendations to help the patient improve their vision tracking ability, write these points as if you are speaking directly to the patient, without any headings or subheadings just point.');
+    ResponseModel impact = await ml.getData(
+        'Now provide three potential impacts that this level of vision tracking ability might have on the patient’s daily activities. provide three impacts only, write them as if you are directly advising the patient, without adding any headings or subheadings just points.');
+    String date = getCurrentDateString();
+    print(impact.text);
+    print(recommendation.text);
+    TestResultModel data = TestResultModel(
+        patientName: 'Bilal Khan',
+        date: date,
+        testType: 'Vision Acuity Test',
+        testName: 'Animal Track',
+        testScore: score1 + score2 + score3,
+        resultDescription: analysis,
+        recommendation: recommendation.text,
+        precautions: impact.text);
+    bool flag = await testRepo.addTestRecord(data);
+    print(flag);
+    emit(AnimalTrackScoreLoading());
+    emit(AnimalTrackScoreLoaded());
+  }
+
+  String determineVisionAcuity(int totalScore) {
+    if (totalScore >= 50) {
+      return "Excellent Vision Acuity: You have excellent vision tracking skills across all difficulty levels.";
+    } else if (totalScore >= 40) {
+      return "Good Vision Acuity: You have good vision tracking skills. You might struggle a bit at higher difficulties.";
+    } else if (totalScore >= 25) {
+      return "Average Vision Acuity: Your vision tracking skills are average. Higher speeds and smaller sizes are challenging.";
+    } else if (totalScore >= 10) {
+      return "Below Average Vision Acuity: You might have difficulty tracking moving objects at higher speeds and smaller sizes.";
+    } else {
+      return "Poor Vision Acuity: You have significant difficulty tracking moving objects. Consider consulting an eye specialist.";
+    }
   }
 }
