cf7e8ac624fb55c17972d67451df0320a51c8743
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-12 20:43:14 +0500

    added global therapy list

--- lib/configs/app/app_globals.dart
+++ lib/configs/app/app_globals.dart
@@ -1,3 +1,5 @@
+import 'package:OculaCare/data/models/therapy/therapy_results_model.dart';
+
 import '../../data/models/disease_result/disease_result_model.dart';
 
 class AppGlobals {
@@ -6,3 +8,4 @@ class AppGlobals {
 String ipAddress = 'http://192.168.0.105:8080';
 Set<DiseaseResultModel> globalResults = {};
 String ipServer= 'https://oculacare-backend.onrender.com';
+List <TherapyModel> globalTherapies = [];
