9d25460f33c1d58d0f0e3fbf88b39fdac33481ce
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-13 01:24:14 +0500

    updated: progress-section screen

--- lib/presentation/therapy/sections/progression_section.dart
+++ lib/presentation/therapy/sections/progression_section.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/material.dart';
 import '../../../configs/presentation/constants/colors.dart';
+import '../../test_dashboard/widgets/test_progress_heatmap.dart';
 
 class ProgressionSection extends StatelessWidget {
   final double screenHeight;
   final double screenWidth;
 
-  const ProgressionSection({
+  const ProgressionSection({super.key,
     required this.screenHeight,
     required this.screenWidth,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Therapy Progression',
-          style: TextStyle(
-              fontFamily: 'MontserratMedium',
-              fontWeight: FontWeight.w800,
-              fontSize: screenWidth * 0.045,
-              color: AppColors.appColor),
-        ),
-        SizedBox(height: screenHeight * 0.008),
-        Center(
-          child: Text(
-            'Track your progress here',
-            style: TextStyle(
-              color: AppColors.textPrimary,
-              fontSize: screenWidth * 0.045,
-            ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: screenHeight * 0.02,
           ),
-        ),
-      ],
+          Text(
+            'Tests Progress',
+            style: TextStyle(
+                fontFamily: 'MontserratMedium',
+                fontWeight: FontWeight.w800,
+                fontSize: screenWidth * 0.045,
+                color: AppColors.appColor),
+          ),
+          SizedBox(
+            height: screenHeight * 0.03,
+          ),
+          // Center(
+          //     child: ProgressCalendarScreen(
+          //         data: state.progressData)),
+          SizedBox(
+            height: screenHeight * 0.035,
+          ),
+        ],
+      ),
     );
   }
 }
