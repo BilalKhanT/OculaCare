29980c3400724131c15eb87e59d94ed849a98356
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-12 10:47:28 +0500

    updated: test progression cubit

--- lib/logic/tests/test_progression_cubit.dart
+++ lib/logic/tests/test_progression_cubit.dart
@@ -11,19 +11,27 @@ class TestProgressionCubit extends Cubit<TestProgressionState> {
 
   void toggleProgression(String testName) {
     String test = '';
+    selectedTest = testName;
     if (testName == 'Contrast Test') {
       test = 'Contrast Sensitivity';
     } else {
       test = testName;
     }
-    selectedTest = testName;
     Map<DateTime, double> scores = {};
     final DateFormat dateFormat = DateFormat('dd-MM-yyyy');
+    final filteredResults = testResults
+        .where((t) => t.testName == test)
+        .toList()
+      ..sort((a, b) =>
+          dateFormat.parse(a.date).compareTo(dateFormat.parse(b.date)));
 
-    for (var t in testResults.where((t) => t.testName == test)) {
+    print(filteredResults.length);
+
+    for (var t in filteredResults) {
       DateTime testDate = dateFormat.parse(t.date);
       scores[testDate] = t.testScore.toDouble();
     }
+
     emit(TestProgressionToggled(scores));
   }
 }
