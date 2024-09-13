ebccb3118fddcb606ebbfcca4f287ea6cf69d689
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-13 20:38:01 +0500

    updated: colors

--- lib/configs/presentation/constants/colors.dart
+++ lib/configs/presentation/constants/colors.dart
@@ -6,9 +6,30 @@ class AppColors {
   static const appColor = Color(0xFF034694);
   static const whiteColor = Color(0xFFFFFFFF);
   static final textGrey = Colors.grey.shade700;
-  static const Color textPrimary = Colors.black; // Black
-  static const Color textSecondary = Colors.black54; // Light Black/Gray
-  static const Color backgroundTherapy = Color(0xFF1E1B24); // therapy color
+  static const Color textPrimary = Colors.black;
+  static const Color textSecondary = Colors.black54;
+  static const Color backgroundTherapy = Color(0xFF1E1B24);
   static const Color textTherapy = Color(0xFFfef3d6);
   static const secondaryText = Color(0xFF636363);
+
+  static const Color lightBlue = Color(0xFFADD8E6);
+  static const Color darkBlue = Color(0xFF00008B);
+  static const Color brightRed = Color(0xFFFF0000);
+  static const Color lightGreen = Color(0xFF90EE90);
+  static const Color darkGreen = Color(0xFF006400);
+  static const Color lightYellow = Color(0xFFFFFF99);
+  static const Color purple = Color(0xFF800080);
+  static const Color orange = Color(0xFFFFA500);
+  static const Color pink = Color(0xFFFFC0CB);
+  static const Color lightGray = Color(0xFFD3D3D3);
+  static const Color deepOrange = Color(0xFFFF8C00);
+  static const Color teal = Color(0xFF008080);
+
+  static const Color contentColorGreen = Color(0xFF00C853);  // Added Green Color
+  static const Color contentColorPurple = Color(0xFF9C27B0); // Purple
+  static const Color contentColorYellow = Color(0xFFFFEB3B); // Yellow
+  static const Color contentColorBlue = Color(0xFF2196F3);   // Blue
+  static const Color contentColorOrange = Color(0xFFFF9800); // Orange
+  static const Color contentColorPink = Color(0xFFE91E63);   // Pink
+  static const Color contentColorRed = Color(0xFFF44336);
 }
