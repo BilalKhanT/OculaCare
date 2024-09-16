f623f50b6f0505e232ca7eaf38ee7ca8bafe0b34
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-16 19:34:43 +0500

    updated: disease result model

--- lib/data/models/disease_result/disease_result_model.dart
+++ lib/data/models/disease_result/disease_result_model.dart
@@ -1,13 +1,36 @@
 class DiseaseResultModel {
+  final String patientName;
+  final String date;
+  final String treatment;
+  final String causes;
+  final String medicineRecommendations;
+  final String impacts;
+  final String precautions;
   final EyePrediction leftEye;
   final EyePrediction rightEye;
 
-  DiseaseResultModel({required this.leftEye, required this.rightEye});
+  DiseaseResultModel(
+      {required this.patientName,
+      required this.date,
+      required this.treatment,
+      required this.causes,
+      required this.medicineRecommendations,
+      required this.impacts,
+      required this.precautions,
+      required this.leftEye,
+      required this.rightEye});
 
   factory DiseaseResultModel.fromJson(Map<String, dynamic> json) {
     return DiseaseResultModel(
       leftEye: EyePrediction.fromJson(json['left_eye']),
       rightEye: EyePrediction.fromJson(json['right_eye']),
+      patientName: json['patient_name'],
+      date: json['date'],
+      treatment: json['treatment'],
+      causes: json['causes'],
+      medicineRecommendations: json['medicine_recommendations'],
+      impacts: json['impacts'],
+      precautions: json['precautions'],
     );
   }
 }
