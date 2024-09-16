eb584a4ef6ab0633dc698447d8d4debacb51bbe3
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-16 19:35:34 +0500

    updated: test dashboard

--- lib/presentation/test_dashboard/test_dash_view.dart
+++ lib/presentation/test_dashboard/test_dash_view.dart
@@ -146,14 +146,6 @@ class TestDashView extends StatelessWidget {
                                   ? AppColors.appColor.withOpacity(0.85)
                                   : Colors.white,
                               borderRadius: BorderRadius.circular(25),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 1,
-                                  blurRadius: 6,
-                                  offset: const Offset(0, 3),
-                                ),
-                              ],
                             ),
                             padding: const EdgeInsets.symmetric(
                                 horizontal: 15.0, vertical: 6.0),
@@ -184,14 +176,6 @@ class TestDashView extends StatelessWidget {
                                   ? AppColors.appColor.withOpacity(0.85)
                                   : Colors.white,
                               borderRadius: BorderRadius.circular(25),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 1,
-                                  blurRadius: 6,
-                                  offset: const Offset(0, 3),
-                                ),
-                              ],
                             ),
                             padding: const EdgeInsets.symmetric(
                                 vertical: 6.0, horizontal: 10.0),
@@ -225,14 +209,6 @@ class TestDashView extends StatelessWidget {
                                   ? AppColors.appColor.withOpacity(0.85)
                                   : Colors.white,
                               borderRadius: BorderRadius.circular(50),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 1,
-                                  blurRadius: 6,
-                                  offset: const Offset(0, 3),
-                                ),
-                              ],
                             ),
                             padding: const EdgeInsets.symmetric(
                                 vertical: 6.0, horizontal: 10.0),
