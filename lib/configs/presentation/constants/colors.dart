7c7587f9332463c51a66428171e17206dadc1720
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-16 19:33:41 +0500

    updated: colors class

--- lib/configs/presentation/constants/colors.dart
+++ lib/configs/presentation/constants/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
-  static const screenBackground = Color(0xFFF9FAFB);
+  static const screenBackground = Color(0xFFF1F3F4);
   static const appColor = Color(0xFF034694);
   static const whiteColor = Color(0xFFFFFFFF);
   static final textGrey = Colors.grey.shade700;
