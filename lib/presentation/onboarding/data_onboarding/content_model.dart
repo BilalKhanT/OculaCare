57001f499ec6bac17b43a86200664b9e02cdce2f
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-24 13:35:41 +0500

    updated: content modal class

--- lib/presentation/onboarding/data_onboarding/content_model.dart
+++ lib/presentation/onboarding/data_onboarding/content_model.dart
@@ -5,37 +5,3 @@ class OnBoardingContent {
 
   OnBoardingContent({this.image, this.title, this.discription});
 }
-
-class EducationModel {
-  final String title, description, image;
-
-  EducationModel({
-    required this.title,
-    required this.description,
-    required this.image,
-  });
-}
-
-
-List<EducationModel> education = [
-  EducationModel(
-    title: "Eye Health Tips",
-    description: "",
-    image: 'assets/images/edu_1.jpeg',
-  ),
-  EducationModel(
-    title: "Eye Health Tips",
-    description: "",
-    image: 'assets/images/edu_3.jpeg',
-  ),
-  EducationModel(
-    title: "Eye Health Tips",
-    description: "",
-    image: 'assets/images/edu_2.jpeg',
-  ),
-  EducationModel(
-    title: "Eye Health Tips",
-    description: "",
-    image: 'assets/images/edu_1.jpeg',
-  ),
-];
