68e4d3d596aa019008e6984c01c53ad5352c0d70
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-25 13:04:56 +0500

    fix: animal track level and score not updating error

--- lib/logic/tests/vision_tests/animal_track_cubit.dart
+++ lib/logic/tests/vision_tests/animal_track_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:OculaCare/configs/app/app_globals.dart';
 import 'package:OculaCare/data/models/tests/score_model.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,17 +7,20 @@ class AnimalTrackState {
   final int score;
   final int mistaps;
   final bool isGameOver;
+  final int level;
 
-  AnimalTrackState({this.score = 0, this.mistaps = 0, this.isGameOver = false});
+  AnimalTrackState({this.score = 0, this.mistaps = 0, this.isGameOver = false, this.level = 0});
 
-  AnimalTrackState copyWith({int? score, int? mistaps, bool? isGameOver}) {
+  AnimalTrackState copyWith({int? score, int? mistaps, bool? isGameOver, int? level}) {
     return AnimalTrackState(
       score: score ?? this.score,
       mistaps: mistaps ?? this.mistaps,
       isGameOver: isGameOver ?? this.isGameOver,
+      level: level ?? this.level,
     );
   }
 }
+
 
 class AnimalTrackCubit extends Cubit<AnimalTrackState> {
   AnimalTrackCubit() : super(AnimalTrackState());
@@ -27,42 +31,46 @@ class AnimalTrackCubit extends Cubit<AnimalTrackState> {
   int levelOneScore = 0;
   int levelTwoScore = 0;
   int levelThreeScore = 0;
-  int level = 0;
   int score = 0;
 
   Future<void> startGame() async {
+    trackLevel = 0;
+    levelOneScore = 0;
+    levelTwoScore = 0;
+    levelThreeScore = 0;
+    score = 0;
     await _bgAudioPlayer.setSource(AssetSource('audio/good-night-160166.mp3'));
     await _bgAudioPlayer.setVolume(0.1);
     _bgAudioPlayer.setReleaseMode(ReleaseMode.loop);
     await _bgAudioPlayer.resume();
   }
 
-  void updateLevel() {
-    level = level + 1;
-  }
-
   void incrementScore() async {
+    await _successAudioPlayer.stop();
+    await _errorAudioPlayer.stop();
     await _successAudioPlayer.setSource(AssetSource('audio/success.mp3'));
     await _successAudioPlayer.setPlaybackRate(2.0);
     await _successAudioPlayer.resume();
-    score = state.score + 1;
-    if (level == 0) {
-      if (levelOneScore <= 20) {
-        levelOneScore = levelOneScore + 1;
+
+    if (trackLevel == 0) {
+      if (levelOneScore < 20) {
+        levelOneScore++;
       }
-    } else if (level == 1) {
-      if (levelTwoScore <= 20) {
-        levelTwoScore = levelTwoScore + 1;
+    } else if (trackLevel == 1) {
+      if (levelTwoScore < 20) {
+        levelTwoScore++;
       }
-    } else if (level == 2) {
-      if (levelThreeScore <= 20) {
-        levelThreeScore = levelThreeScore + 1;
+    } else if (trackLevel == 2) {
+      if (levelThreeScore < 20) {
+        levelThreeScore++;
       }
     }
     emit(state.copyWith(score: state.score + 1));
   }
 
   void incrementMistaps() async {
+    await _errorAudioPlayer.stop();
+    await _successAudioPlayer.stop();
     await _errorAudioPlayer.setSource(AssetSource('audio/error.mp3'));
     await _errorAudioPlayer.setPlaybackRate(2.0);
     await _errorAudioPlayer.resume();
@@ -81,7 +89,7 @@ class AnimalTrackCubit extends Cubit<AnimalTrackState> {
     _successAudioPlayer.stop();
     ScoreModel scoreModel = ScoreModel(
         score1: levelOneScore, score2: levelTwoScore, score3: levelThreeScore);
-    level = 0;
+    trackLevel = 0;
     levelOneScore = 0;
     levelTwoScore = 0;
     levelThreeScore = 0;
