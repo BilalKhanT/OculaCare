d509eb54505696795b1553b6c8fa4138591812a5
awais-ur-rehman <awaisjarral37@gmail.com>
2024-09-25 23:56:02 +0500

    updated: therapy form

---
+++ lib/data/models/therapy/therapy_feedback_model.dart
@@ -0,0 +1,39 @@
+import 'package:OculaCare/data/models/therapy/therapy_results_model.dart';
+
+class TherapyFeedbackModel {
+  final String email;
+  final TherapyModel therapy;
+  final String category;
+  final List<String> defaults;
+  final String? customMessage;
+
+  TherapyFeedbackModel({
+    required this.email,
+    required this.therapy,
+    required this.category,
+    required this.defaults,
+    this.customMessage,
+  });
+
+
+  factory TherapyFeedbackModel.fromJson(Map<String, dynamic> json) {
+    return TherapyFeedbackModel(
+      email: json['email'] ?? '',
+      therapy: TherapyModel.fromJson(json['therapy'] ?? {}),
+      category: json['category'] ?? '',
+      defaults: List<String>.from(json['defaults'] ?? []),
+      customMessage: json['customMessage'],
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'therapy': therapy.toJson(),
+      'category': category,
+      'defaults': defaults,
+      'customMessage': customMessage,
+    };
+  }
+}
