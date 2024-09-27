2f489e60ba35bab3d48e3e26c140bdaeec1358e3
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 13:28:01 +0500

    added: stt state

---
+++ lib/logic/tests/vision_tests/stt_state.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+
+class SttState extends Equatable {
+  const SttState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class SttInitial extends SttState {}
+
+class SttListening extends SttState {}
