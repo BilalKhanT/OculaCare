b83b7fda145db3fd203561dbee206eebd4ec2d86
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 23:35:47 +0500

    chore: refactored code

--- lib/data/models/patient/patient_model.dart
+++ lib/data/models/patient/patient_model.dart
@@ -8,12 +8,12 @@ class PatientModel {
   });
 
   factory PatientModel.fromJson(Map<String, dynamic> json) => PatientModel(
-    patient: Patient.fromJson(json["patient"]),
-  );
+        patient: Patient.fromJson(json["patient"]),
+      );
 
   Map<String, dynamic> toJson() => {
-    "patient": patient.toJson(),
-  };
+        "patient": patient.toJson(),
+      };
 }
 
 class Patient {
@@ -36,22 +36,23 @@ class Patient {
   });
 
   factory Patient.fromJson(Map<String, dynamic> json) => Patient(
-    email: json["email"],
-    username: json["username"],
-    profileImage: json["profileImage"],
-    age: json["age"],
-    gender: json["gender"],
-    contactNumber: json["contactNumber"],
-    address: json["address"] != null ? Address.fromJson(json["address"]) : null,
-  );
+        email: json["email"],
+        username: json["username"],
+        profileImage: json["profileImage"],
+        age: json["age"],
+        gender: json["gender"],
+        contactNumber: json["contactNumber"],
+        address:
+            json["address"] != null ? Address.fromJson(json["address"]) : null,
+      );
 
   Map<String, dynamic> toJson() => {
-    "email": email,
-    "username": username,
-    "profileImage": profileImage,
-    "age": age,
-    "gender": gender,
-    "contactNumber": contactNumber,
-    "address": address?.toJson(),
-  };
+        "email": email,
+        "username": username,
+        "profileImage": profileImage,
+        "age": age,
+        "gender": gender,
+        "contactNumber": contactNumber,
+        "address": address?.toJson(),
+      };
 }
