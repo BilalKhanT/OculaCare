0f30d421900ca3a9ff04acaa55cae395c120f7de
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 23:01:16 +0500

    chore: refactored code

--- lib/logic/sign_up_cubit/sign_up_cubit.dart
+++ lib/logic/sign_up_cubit/sign_up_cubit.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
+import 'package:cculacare/logic/sign_up_cubit/sign_up_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:OculaCare/logic/sign_up_cubit/sign_up_state.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
