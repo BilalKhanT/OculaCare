78ab108f80263e90dac2e2589880c9ea4bb23fe9
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 21:47:33 +0500

    chore: removed unused imports

--- lib/presentation/widgets/need_to_setup_profile_widget.dart
+++ lib/presentation/widgets/need_to_setup_profile_widget.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
-import '../../configs/presentation/constants/colors.dart';
 import '../../configs/routes/route_names.dart';
 
 class NeedToSetupProfileWidget extends StatelessWidget {
