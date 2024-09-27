303b5e917d342f28d12ad5abac2fdfd9403fcda4
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 23:37:45 +0500

    chore: refactored code

--- lib/presentation/login/widgets/forgot_password_form.dart
+++ lib/presentation/login/widgets/forgot_password_form.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -72,7 +71,8 @@ class ForgotPasswordForm extends StatelessWidget {
                   return;
                 }
                 if (context.mounted) {
-                  context.read<OtpCubit>().sendRecoveryOTP(loginCubit.recoveryEmailController.text.trim());
+                  context.read<OtpCubit>().sendRecoveryOTP(
+                      loginCubit.recoveryEmailController.text.trim());
                   loginCubit.recoveryEmailController.clear();
                   context.push(RouteNames.otpRoute, extra: 'recover');
                 }
