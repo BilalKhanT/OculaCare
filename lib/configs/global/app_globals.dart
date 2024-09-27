662e181ca327051523680168adc7bfd2ab23a904
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 21:37:03 +0500

    updated: app-globals

--- lib/configs/global/app_globals.dart
+++ lib/configs/global/app_globals.dart
@@ -1,5 +1,26 @@
-import 'package:OculaCare/data/models/tests/test_result_model.dart';
+import 'package:OculaCare/data/models/therapy/therapy_results_model.dart';
 
-class AppGlobals {}
+import '../../data/models/disease_result/disease_result_model.dart';
+import '../../data/models/tests/test_result_model.dart';
 
+class AppGlobals {
+
+}
+String ipAddress = 'http://192.168.0.105:8080';
+String ipServer= 'https://oculacare-backend.onrender.com';
+List<DiseaseResultModel> globalResults = [];
+List <TherapyModel> globalTherapies = [];
+Map<DateTime, int> globalTherapyProgressData = {};
+Map<String, int> barChartData = {};
+Map<String, Map<DateTime, int>> categoryDateTherapyCount = {};
 List<TestResultModel> testResults = [];
+int trackLevel = 0;
+
+void clearGlobalDataOnLogout() {
+  testResults.clear();
+  globalResults.clear();
+  globalTherapies.clear();
+  globalTherapyProgressData.clear();
+  barChartData.clear();
+  categoryDateTherapyCount.clear();
+}
