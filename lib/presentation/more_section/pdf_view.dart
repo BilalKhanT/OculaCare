50921ae5ae4729fe1d12926c3da647988dacaaf5
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 21:47:37 +0500

    chore: removed unused imports

--- lib/presentation/more_section/pdf_view.dart
+++ lib/presentation/more_section/pdf_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import '../../logic/pdf_cubit/pdf_cubit_state.dart';
 
