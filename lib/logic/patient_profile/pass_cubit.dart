660618574064e58a8db5d04caee5ff78cf024420
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 23:17:33 +0500

    chore: refactored code

--- lib/logic/patient_profile/pass_cubit.dart
+++ lib/logic/patient_profile/pass_cubit.dart
@@ -1,8 +1,9 @@
-import 'package:OculaCare/data/repositories/local/preferences/shared_prefs.dart';
-import 'package:OculaCare/data/repositories/patient/password_repo.dart';
-import 'package:OculaCare/logic/patient_profile/pass_state.dart';
 import 'package:bloc/bloc.dart';
+import 'package:cculacare/logic/patient_profile/pass_state.dart';
 import 'package:flutter/cupertino.dart';
+
+import '../../data/repositories/local/preferences/shared_prefs.dart';
+import '../../data/repositories/patient/password_repo.dart';
 
 class PassCubit extends Cubit<PassState> {
   PassCubit() : super(PassInitial());
