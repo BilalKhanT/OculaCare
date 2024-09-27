4dd0acdfd0e832af4b5b0ac71c894b2ae654f841
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-27 23:14:06 +0500

    chore: refactored code

--- lib/presentation/therapy/sections/therapy_section.dart
+++ lib/presentation/therapy/sections/therapy_section.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:OculaCare/presentation/therapy/widgets_therapy/general_eye_exercises.dart';
 import '../../../configs/presentation/constants/colors.dart';
 import '../../../data/therapies_data/therapies_list.dart';
+import '../widgets_therapy/general_eye_exercises.dart';
 import '../widgets_therapy/recommended_therapy_list.dart';
 
 class TherapySection extends StatelessWidget {
