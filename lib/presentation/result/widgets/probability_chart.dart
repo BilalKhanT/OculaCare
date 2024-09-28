29fcfcdeb3035d9cebeef8f969885a10c673ccea
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-28 22:44:15 +0500

    updated: probability chart widget

--- lib/presentation/result/widgets/probability_chart.dart
+++ lib/presentation/result/widgets/probability_chart.dart
@@ -1,6 +1,8 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
+import '../../../configs/presentation/constants/colors.dart';
+
 class ProbabilityChart extends StatelessWidget {
   final double score;
 
@@ -11,33 +13,47 @@ class ProbabilityChart extends StatelessWidget {
     double finalScore = 0.0;
     Color severityColor = _getSeverityColor(score);
     double screenWidth = MediaQuery.sizeOf(context).width;
+    double screenHeight = MediaQuery.sizeOf(context).height;
     if (score < 0) {
     } else {
       finalScore = score;
     }
     return Column(
       children: [
-        SizedBox(
-          height: 200,
+        Container(
+          height: screenHeight * 0.22,
+          width: screenHeight * 0.22,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100.0),
+            color: Colors.transparent,
+            boxShadow:  [
+              BoxShadow(
+                color: Colors.grey.shade300,
+                spreadRadius: 0.5,
+                blurRadius: 7,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
           child: PieChart(
             PieChartData(
               sections: [
                 PieChartSectionData(
                   value: finalScore.toDouble(),
                   color: severityColor,
-                  radius: 40,
+                  radius: screenHeight * 0.07,
                   title: '$finalScore',
                   titleStyle: TextStyle(
                     fontFamily: 'MontserratMedium',
                     fontWeight: FontWeight.w800,
                     fontSize: screenWidth * 0.04,
-                    color: Colors.white,
+                    color: Colors.black,
                   ),
                 ),
                 PieChartSectionData(
                   value: (100.0 - finalScore),
                   color: Colors.grey.shade700,
-                  radius: 30,
+                  radius: screenHeight * 0.06,
                   titleStyle: TextStyle(
                     fontFamily: 'MontserratMedium',
                     fontWeight: FontWeight.w800,
