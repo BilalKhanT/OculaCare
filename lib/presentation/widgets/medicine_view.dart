8c706ca09eee37a647e6c3f57077c8362baa68af
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-29 21:59:48 +0500

    updated: med view

--- lib/presentation/widgets/medicine_view.dart
+++ lib/presentation/widgets/medicine_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class MedicineView extends StatelessWidget {
   const MedicineView({super.key});
@@ -9,21 +8,6 @@ class MedicineView extends StatelessWidget {
     double screenHeight = MediaQuery.sizeOf(context).height;
     double screenWidth = MediaQuery.sizeOf(context).width;
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                      height: screenHeight * 0.5,
-                      width: screenWidth,
-                child: const WebView(
-                  initialUrl: 'https://app.vectary.com/p/4pWRcBqOMjR8vy33R2Tf68',
-                  javascriptMode: JavascriptMode.unrestricted,
-
-                ),
-                    ),
-            ],
-          )),
     );
   }
 }
