0319d910f69fb42567fbdf8f11d484763ff77ed6
BilalKhanT <m.bilalkhant@gmail.com>
2024-09-30 21:30:32 +0500

    added: med model

---
+++ lib/data/models/disease_result/medicine_model.dart
@@ -0,0 +1,15 @@
+class MedicineModel {
+  final String image;
+  final String name;
+  final String dosage;
+  final String usage;
+  final int duration;
+
+  MedicineModel({
+    required this.image,
+    required this.name,
+    required this.dosage,
+    required this.usage,
+    required this.duration,
+  });
+}
