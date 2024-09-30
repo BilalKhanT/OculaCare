3df9f7d264e7c8c07d900cdb0a5facda65d829cb
BilalKhanT <m.bilalkhant@gmail.com>
2024-10-01 03:40:47 +0500

    updated: route names

--- lib/configs/routes/route_names.dart
+++ lib/configs/routes/route_names.dart
@@ -34,4 +34,5 @@ class RouteNames {
   static const String therapyFeedbackRoute = '/therapyFeedbackRoute';
   static const String medicineRoute = '/medicineRoute';
   static const String diagnosisRoute = '/diagnosisRoute';
+  static const String questionRoute = '/questionRoute';
 }
