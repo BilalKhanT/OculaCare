6ca6da0f1e5cbc6002e8d5bd834cc57c61374a5a
BilalKhanT <m.bilalkhant@gmail.com>
2024-10-01 11:11:42 +0500

    updated: detect repo

--- lib/data/repositories/detection/detection_repo.dart
+++ lib/data/repositories/detection/detection_repo.dart
@@ -6,8 +6,30 @@ import 'package:cculacare/data/repositories/local/preferences/shared_prefs.dart'
 import 'package:nb_utils/nb_utils.dart';
 import 'package:http/http.dart' as http;
 
+import '../../../configs/app/notification/notification_service.dart';
+
 class DetectionRepo {
-  final String detectionUrl = '${ipServer}hhdhd';
+  final String detectionUrl = '$ipServer/api/predictions/email';
+
+  Future<void> predictDisease(Map<String, dynamic> payload) async {
+    try {
+      var response = await http.post(
+        Uri.parse('$flask/predict'),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(payload),
+      );
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        DiseaseResultModel result = DiseaseResultModel.fromJson(data);
+        globalResults.add(result);
+        NotificationService.resultReadyNotification('Disease Analysis', 'Analysis report is ready', DateTime.now().add(const Duration(seconds: 2)));
+      } else {
+        log("Nothing ${response.body}");
+      }
+    } catch (e) {
+      log('error $e');
+    }
+  }
 
   Future<List<DiseaseResultModel>> getPatientDiseaseResults() async {
     final url = Uri.parse('$detectionUrl/${sharedPrefs.email}');
