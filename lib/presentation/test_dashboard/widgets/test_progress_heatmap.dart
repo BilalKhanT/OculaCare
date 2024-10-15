1c443d96edc0ab7ab3e1ad0d1af931b5c7d34d97
BilalKhanT <m.bilalkhant@gmail.com>
2024-10-15 17:27:00 +0500

    updated: heatmap

--- lib/presentation/test_dashboard/widgets/test_progress_heatmap.dart
+++ lib/presentation/test_dashboard/widgets/test_progress_heatmap.dart
@@ -15,12 +15,12 @@ class ProgressCalendarScreen extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(8.0),
-        boxShadow: const [
+        boxShadow: [
           BoxShadow(
-            color: AppColors.appColor,
+            color: AppColors.appColor.withOpacity(0.3),
             spreadRadius: 1,
             blurRadius: 0.5,
-            offset: Offset(0, 0),
+            offset: const Offset(0, 0),
           ),
         ],
       ),
