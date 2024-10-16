05316bdf6278636e5f09c6f92335a2b4990a37c8
BilalKhanT <m.bilalkhant@gmail.com>
2024-10-16 22:19:24 +0500

    updated: add tile

--- lib/presentation/patient_profile/widgets/address_tile.dart
+++ lib/presentation/patient_profile/widgets/address_tile.dart
@@ -1,18 +1,28 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../../configs/presentation/constants/colors.dart';
+import '../../../configs/utils/utils.dart';
+import '../../../data/models/address/address_model.dart';
+import '../../../logic/address_book/address_book_cubit.dart';
 
 class AddressTile extends StatelessWidget {
-  const AddressTile({super.key});
+  final Address address;
+  final bool isSelected;
+
+  const AddressTile({
+    super.key,
+    required this.address,
+    required this.isSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.sizeOf(context).width;
     return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: 4.0, vertical: 9.0),
+      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 9.0),
       child: FadeIn(
         duration: const Duration(milliseconds: 1000 + (5 * 100)),
         child: Slidable(
@@ -21,12 +31,26 @@ class AddressTile extends StatelessWidget {
             motion: const StretchMotion(),
             children: [
               SlidableAction(
-                onPressed: (context) {},
-                icon: Icons.flag_outlined,
+                onPressed: (context) {
+                  context.read<AddressBookCubit>().selectCurrentAddress(address);
+                },
+                icon: Icons.location_on_outlined,
                 backgroundColor: AppColors.appColor.withOpacity(0.7),
               ),
               SlidableAction(
-                onPressed: (context) {},
+                onPressed: (context) async {
+                  final bool flag =
+                  await context.read<AddressBookCubit>().deleteAddress(address);
+                  if (context.mounted) {
+                    if (flag) {
+                      AppUtils.showToast(context, 'Address Removed',
+                          'Address has been removed successfully', false);
+                      return;
+                    }
+                    AppUtils.showToast(context, 'Error Occurred',
+                        'Failed to successfully remove address', true);
+                  }
+                },
                 icon: Icons.delete_outlined,
                 backgroundColor: const Color(0xffB81736),
               )
@@ -38,7 +62,14 @@ class AddressTile extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(9.0),
-                boxShadow: [
+                boxShadow: isSelected ? [
+                BoxShadow(
+                  color: AppColors.appColor.withOpacity(0.9),
+                  spreadRadius: 1,
+                  blurRadius: 0.5,
+                  offset: const Offset(0, 0),
+                ),
+                  ] : [
                   BoxShadow(
                     color: AppColors.textPrimary.withOpacity(0.1),
                     spreadRadius: 1,
@@ -48,15 +79,14 @@ class AddressTile extends StatelessWidget {
                 ],
               ),
               child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 10.0, vertical: 17.0),
+                padding:
+                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 17.0),
                 child: Row(
-                  mainAxisAlignment:
-                  MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
                     Flexible(
                       child: Text(
-                        'Title Description hdebdiediendendeinden',
+                        '${address.locationName}',
                         style: TextStyle(
                           fontFamily: 'MontserratMedium',
                           fontWeight: FontWeight.w800,
@@ -68,23 +98,21 @@ class AddressTile extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(
-                          right: 10.0, left: 4),
+                      padding: const EdgeInsets.only(right: 10.0, left: 4),
                       child: Container(
                         decoration: BoxDecoration(
-                          borderRadius:
-                          BorderRadius.circular(100.0),
+                          borderRadius: BorderRadius.circular(100.0),
                           color: AppColors.appColor.withOpacity(0.2),
                         ),
                         child: Padding(
                           padding: const EdgeInsets.all(1.0),
                           child: IconButton(
                             onPressed: () {
-                              Slidable.of(context)
-                                  ?.openEndActionPane();
+                              Slidable.of(context)?.openEndActionPane();
                             },
-                            icon:
-                            const Icon(Icons.tune_outlined,),
+                            icon: const Icon(
+                              Icons.navigate_next_outlined,
+                            ),
                             color: AppColors.appColor,
                             iconSize: 23,
                           ),
