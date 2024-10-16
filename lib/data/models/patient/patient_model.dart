4b405797a4c826cbe980fa85b03be48a75316713
BilalKhanT <m.bilalkhant@gmail.com>
2024-10-16 22:17:27 +0500

    updated: patient model

--- lib/data/models/patient/patient_model.dart
+++ lib/data/models/patient/patient_model.dart
@@ -2,18 +2,24 @@ import '../address/address_model.dart';
 
 class PatientModel {
   final Patient patient;
+  final List<Address> addressBook;
 
   PatientModel({
     required this.patient,
+    required this.addressBook,
   });
 
   factory PatientModel.fromJson(Map<String, dynamic> json) => PatientModel(
-        patient: Patient.fromJson(json["patient"]),
-      );
+    patient: Patient.fromJson(json["patient"]),
+    addressBook: (json["addressBook"] as List<dynamic>)
+        .map((address) => Address.fromJson(address as Map<String, dynamic>))
+        .toList(),
+  );
 
   Map<String, dynamic> toJson() => {
-        "patient": patient.toJson(),
-      };
+    "patient": patient.toJson(),
+    "addressBook": addressBook.map((address) => address.toJson()).toList(),
+  };
 }
 
 class Patient {
@@ -36,23 +42,23 @@ class Patient {
   });
 
   factory Patient.fromJson(Map<String, dynamic> json) => Patient(
-        email: json["email"],
-        username: json["username"],
-        profileImage: json["profileImage"],
-        age: json["age"],
-        gender: json["gender"],
-        contactNumber: json["contactNumber"],
-        address:
-            json["address"] != null ? Address.fromJson(json["address"]) : null,
-      );
+    email: json["email"],
+    username: json["username"],
+    profileImage: json["profileImage"],
+    age: json["age"],
+    gender: json["gender"],
+    contactNumber: json["contactNumber"],
+    address:
+    json["address"] != null ? Address.fromJson(json["address"]) : null,
+  );
 
   Map<String, dynamic> toJson() => {
-        "email": email,
-        "username": username,
-        "profileImage": profileImage,
-        "age": age,
-        "gender": gender,
-        "contactNumber": contactNumber,
-        "address": address?.toJson(),
-      };
+    "email": email,
+    "username": username,
+    "profileImage": profileImage,
+    "age": age,
+    "gender": gender,
+    "contactNumber": contactNumber,
+    "address": address?.toJson(),
+  };
 }
