196f23089185305e0327880efe24b45270a7df38
BilalKhanT <m.bilalkhant@gmail.com>
2024-11-16 19:47:39 +0500

    updated: feedback cubit

--- lib/data/repositories/feedback_repo/feedback_repo.dart
+++ lib/data/repositories/feedback_repo/feedback_repo.dart
@@ -1,17 +1,20 @@
 import 'dart:convert';
+import 'package:cculacare/data/repositories/local/preferences/shared_prefs.dart';
 import 'package:http/http.dart' as http;
 import '../../../configs/global/app_globals.dart';
 
 class FeedbackRepository {
   final String apiUrl = '$ipServer/api/feedback/submit';
 
-  Future<bool> submitTherapyFeedback(String email, String category, List<String> data, String customFeedback) async {
+  Future<bool> submitTherapyFeedback(String email, String category, List<String> data, String customFeedback, String date) async {
     try {
       final Map<String, dynamic> feedbackData = {
         'email': email,
+        'patient_name': sharedPrefs.userName,
         'category': category,
         'defaults': data,
         'customMessage': customFeedback,
+        'date': date,
       };
       final response = await http.post(
         Uri.parse(apiUrl),
